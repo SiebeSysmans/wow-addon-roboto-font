69d03ece5ef9e035b8dd219bfbae01d1e54feeff
Siebe Sysmans <1453276+SiebeSysmans@users.noreply.github.com>
2022-11-20 15:23:49 +0100

    Initial project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# wow-addon-roboto-font
-Addon for World of Warcraft that adds the Roboto font.
+# Roboto Font (World of Warcraft addon)
+
+Add the font families [Roboto](https://fonts.google.com/specimen/Roboto), [Roboto Condensed](https://fonts.google.com/specimen/Roboto+Condensed) and [Roboto Slab](https://fonts.google.com/specimen/Roboto+Slab) with variants Bold, Regular and Light for use in other World of Warcraft addons that support the SharedMedia library.
+
+## License
+
+This project is licensed under Apache License 2.0
---
+++ RobotoFont.lua
@@ -0,0 +1,16 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- Roboto
+LSM:Register(LSM.MediaType.FONT, "Roboto-Bold", [[Interface\Addons\RobotoFont\Fonts\Roboto\Roboto-Bold.ttf]])
+LSM:Register(LSM.MediaType.FONT, "Roboto-Regular", [[Interface\Addons\RobotoFont\Fonts\Roboto\Roboto-Regular.ttf]])
+LSM:Register(LSM.MediaType.FONT, "Roboto-Light", [[Interface\Addons\RobotoFont\Fonts\Roboto\Roboto-Light.ttf]])
+
+-- Roboto Condensed
+LSM:Register(LSM.MediaType.FONT, "RobotoCondensed-Bold", [[Interface\Addons\RobotoFont\Fonts\RobotoCondensed\RobotoCondensed-Bold.ttf]])
+LSM:Register(LSM.MediaType.FONT, "RobotoCondensed-Regular", [[Interface\Addons\RobotoFont\Fonts\RobotoCondensed\RobotoCondensed-Regular.ttf]])
+LSM:Register(LSM.MediaType.FONT, "RobotoCondensed-Light", [[Interface\Addons\RobotoFont\Fonts\RobotoCondensed\RobotoCondensed-Light.ttf]])
+
+-- Roboto Slab
+LSM:Register(LSM.MediaType.FONT, "RobotoSlab-Bold", [[Interface\Addons\RobotoFont\Fonts\RobotoSlab\RobotoSlab-Bold.ttf]])
+LSM:Register(LSM.MediaType.FONT, "RobotoSlab-Regular", [[Interface\Addons\RobotoFont\Fonts\RobotoSlab\RobotoSlab-Regular.ttf]])
+LSM:Register(LSM.MediaType.FONT, "RobotoSlab-Light", [[Interface\Addons\RobotoFont\Fonts\RobotoSlab\RobotoSlab-Light.ttf]])
